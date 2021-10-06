77027f1467b9415161b88a1ac3b83b796de46900
Laura Gagliani <gaglianilaura@gmail.com>
2021-10-06 18:16:51 +0200

    finito metodo sorting alternativo

--- Day3.EsercizioArrayDaOrdinare/Program.cs
+++ Day3.EsercizioArrayDaOrdinare/Program.cs
@@ -6,7 +6,7 @@ namespace Day3.EsercizioArrayDaOrdinare
     {
         static void Main(string[] args)
         {
-            //metodo come Paola
+            #region bubble sort (Paola)
 
             int[] ar = new int[] { 3, 1, 10, 9, 5 };
 
@@ -33,31 +33,58 @@ namespace Day3.EsercizioArrayDaOrdinare
                 Console.WriteLine(ar[n]);
             }
 
-
-            //  int[] arOrdinato = new int[ar.Length];
-
-
-
-
-            /*
-            int min = ar[0];
-            int box;
+            #endregion
 
 
 
 
-            for (int i = 1; i < ar.Length; i++)
+            #region selection sort (come avevo pensato io)
+            Console.WriteLine("\n\n");
+
+
+            int[] v = new int[] { 2, 9, 1, 7, 12, 34, 2, 5 };
+
+            int min;
+            int index;
+            int y;
+
+            for (int m = 0; m < v.Length - 1; m++)
             {
-                if (ar[i] < min)
+
+                min = v[m];
+                index = m;   // NB!! inizializzo a 0. questo perché il ciclo for non include nel confronto la posizione v[0], che invece potrebbe essere quella del min
+
+
+
+                for (int n = m+1; n < v.Length; n++) //ciclo che trova il minimo tra gli elementi del vettore
                 {
-                    min = ar[i];
+                    if (v[n] < min) //se trovi qualcosa minore del "min" assunto
+                    {
+                        min = v[n];  //sostituiscine il valore in min
+                        index = n;   //e annotane la posizione all'interno del vettore in questa variabile
+                    }
                 }
 
+                // finito questo for ottengo il valore minimo contenuto nel vettore e la sua posizione
+                // l'obiettivo è ora di scambiare questo elemento con la prima posizione nel vettore
+
+                y = v[m];
+                v[m] = v[index];
+                v[index] = y;   //con questi tre passaggi ho fatto lo scambio, il min è ora in prima posizione
+
+                //tutto quanto ora deve essere ripetuto! inscatolamento in ciclo con 2ndo indice
+                //ripetuto quante volte? quanti sono gli elementi nel vettore - 1, perché l'ultimo mi va a posto "da sé"
 
             }
 
-            Console.WriteLine(min);
-            */
+
+            for (int p = 0; p < v.Length; p++)
+            {
+                Console.WriteLine(v[p]);
+            }
+
+            #endregion
+
 
 
 
