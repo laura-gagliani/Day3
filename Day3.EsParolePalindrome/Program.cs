2699f9d04903bf5299723cef97955c976f08d9b3
Laura Gagliani <gaglianilaura@gmail.com>
2021-10-06 18:27:26 +0200

    esercizio parola palindroma

--- Day3.EsParolePalindrome/Program.cs
+++ Day3.EsParolePalindrome/Program.cs
@@ -6,7 +6,33 @@ namespace Day3.EsParolePalindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("la parola è palindroma?\n");
+
+            char[] parola = new char[] { 'o', 'r', 't', 'o' };
+            // char[] parola = new char[] { 'o', 't', 't', 'o' };
+            //char[] parola = new char[] { 'w', 'o', 'w'};
+
+            int length = parola.Length;
+
+            bool corrispondenza = true;
+
+            for (int i = 0; i <= length/2; i++)
+            {
+                if (parola[i] != parola[length - 1 - i])
+                {
+                    corrispondenza = false;
+                }
+            }
+
+            if(corrispondenza == true)
+            {
+                Console.WriteLine("la parola è palindroma");
+            }
+
+            else
+            {
+                Console.WriteLine("la parola non è palindroma");
+            }
 
 
         }
